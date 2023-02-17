becc0dc446380d9d1a5bf6d989a56c79f78eb34e
Julio Rivas <rivasjulio@ymail.com>
2023-02-17 01:57:07 -0500

    Completed!

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  function moveDodgerRight() {
+    const RightNumbers = dodger.style.left.replace("px", "");
+    const Right = parseInt(RightNumbers, 10);
+  
+    if (Right > 0) {
+      dodger.style.left = `${Right + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
+
